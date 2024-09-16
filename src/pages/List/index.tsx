155426c847d0d32d89b8edf1af8cf2ac04899d1c
devBernardoMendes <devbernardomr@gmail.com>
2024-09-16 18:54:39 -0300

    feat: :sparkles: Creating filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,136 +1,58 @@
 import React from "react";
-import { ContainerSC, ContentHistorySC } from "./styles/styled";
+import { ContainerSC, ContentHistorySC, FiltersSC } from "./styles/styled";
 import ContentHeader from "../../components/ContentHeader";
 import SelectedInput from "../../components/SelectedInput";
 import HistoryCard from "../../components/HistoryCard";
 
 const List: React.FC = () => {
-  const options = [
+  const months = [
     {
-      value: "Option 1",
-      label: "Opção 1",
+      value: 7,
+      label: "Julho",
     },
     {
-      value: "Option 2",
-      label: "Opção 2",
+      value: 8,
+      label: "Agosto",
     },
     {
-      value: "Option 3",
-      label: "Opção 3",
+      value: 9,
+      label: "Setembro",
+    },
+  ];
+
+  const years = [
+    {
+      value: 2022,
+      label: 2022,
+    },
+    {
+      value: 2023,
+      label: 2023,
+    },
+    {
+      value: 2024,
+      label: 2024,
     },
   ];
   return (
     <ContainerSC>
       <ContentHeader title="Saídas" lineColor="#FF6961">
-        <SelectedInput options={options} />
+        <SelectedInput options={months} />
+        <SelectedInput options={years} />
       </ContentHeader>
+      <FiltersSC>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </FiltersSC>
       <ContentHistorySC>
         <HistoryCard
           title="Condomínio - Janeiro"
           subtitle="23/01/2024"
           amount="R$ 550,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Água - Fevereiro"
-          subtitle="15/02/2024"
-          amount="R$ 120,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Internet - Março"
-          subtitle="10/03/2024"
-          amount="R$ 200,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Energia Elétrica - Abril"
-          subtitle="20/04/2024"
-          amount="R$ 450,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Telefone - Maio"
-          subtitle="05/05/2024"
-          amount="R$ 100,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Gás - Junho"
-          subtitle="30/06/2024"
-          amount="R$ 80,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Manutenção - Julho"
-          subtitle="25/07/2024"
-          amount="R$ 300,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Segurança - Agosto"
-          subtitle="12/08/2024"
-          amount="R$ 150,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Imposto Predial - Setembro"
-          subtitle="01/09/2024"
-          amount="R$ 800,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />{" "}
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
-          tagColor="#FF6961"
-        />
-        <HistoryCard
-          title="Seguro Residencial - Outubro"
-          subtitle="18/10/2024"
-          amount="R$ 250,00"
           tagColor="#FF6961"
         />
       </ContentHistorySC>
