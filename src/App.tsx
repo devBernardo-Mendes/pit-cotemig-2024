02ddd8ee642cc7491cdcc17662562a267260fbe9
devBernardoMendes <devbernardomr@gmail.com>
2024-09-17 18:07:36 -0300

    feat: :sparkles: Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-import Layout from "./components/Layout";
 import theme from "./styles/theme/theme";
-import List from "./pages/List";
+import Routes from "./routes";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Layout>
-        <List />
-      </Layout>
+      <Routes />
     </ThemeProvider>
   );
 };
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import List from "../pages/List";
+
+import Layout from "../components/Layout";
+
+const AppRouter = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/condosmart" element={<Dashboard />} />
+        <Route path="/list/:type" element={<List />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default AppRouter;
