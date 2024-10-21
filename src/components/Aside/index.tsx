d80c57835dab4d9b99d0ce9f0e184b914cff1ce8
devBernardoMendes <devbernardomr@gmail.com>
2024-10-21 14:15:21 -0300

    feat: :sparkles: Menu itens

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -9,10 +9,16 @@ import {
 } from "./styles/styled";
 import logoImg from "../../assets/logo.png";
 import {
+  MdAnnouncement,
   MdArrowDownward,
   MdArrowUpward,
+  MdBuild,
   MdDashboard,
+  MdEventAvailable,
   MdExitToApp,
+  MdGroups,
+  MdPeople,
+  MdReportProblem,
 } from "react-icons/md";
 import useLogin from "../../pages/Signin/hooks/useGetLogin";
 
@@ -34,10 +40,39 @@ const Aside: React.FC = () => {
           <MdArrowUpward />
           Entradas
         </MenuIntemLinkSC>
-
         <MenuIntemLinkSC href="/list/exit-balance">
           <MdArrowDownward />
           Saídas
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/residents-registration">
+          <MdPeople />
+          Cadastro de Moradores
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/common-area-reservation">
+          <MdEventAvailable />
+          Reserva de Área Comum
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/complaints">
+          <MdReportProblem />
+          Painel de Reclamações
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/service-requests">
+          <MdBuild />
+          Solicitações de Ordem de Serviço
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/assembly">
+          <MdGroups />
+          Assembleia
+        </MenuIntemLinkSC>
+
+        <MenuIntemLinkSC href="/notice-board">
+          <MdAnnouncement />
+          Mural de Avisos
         </MenuIntemLinkSC>
         <MenuIntemLinkSC href="/login" onClick={logout}>
           <MdExitToApp />
